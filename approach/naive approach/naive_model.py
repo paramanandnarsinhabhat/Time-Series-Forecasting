288d5d13c1e09164fc9b69208d0c0afb94be28a0
paramanandnarsinhabhat <141020967+paramanandnarsinhabhat@users.noreply.github.com>
2024-01-01 11:37:40 +0530

    git ignore added to the project

---
+++ approach/naive approach/naive_model.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+
+import warnings
+warnings.filterwarnings("ignore")
